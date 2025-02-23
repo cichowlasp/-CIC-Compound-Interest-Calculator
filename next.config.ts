ba23a4d4807df5169ed6a11e52c9bcdc02557f61
cichowlasp <cichowlas.p@gmail.com>
2025-02-23 16:05:42 +0100

    reverted back changes

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,5 @@
 import type { NextConfig } from 'next';
 
-const isProd = process.env.NODE_ENV === 'production';
-
-const nextConfig: NextConfig = {
-	reactStrictMode: true,
-	images: {
-		unoptimized: true, // Disable default image optimization
-	},
-	assetPrefix: isProd ? '/your-repository-name/' : '',
-	basePath: isProd ? '/your-repository-name' : '',
-	output: 'export',
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
